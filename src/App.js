0872d0d363d8057cfd32d2f13de34a951612dacd
prashant-freelance <prashant.work.1984@gmail.com>
2020-01-31 14:35:43 +0530

    installer  router and icons  created images folder

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React, { Fragment } from "react";
+// import logo from './logo.svg';
+import "./App.css";
+import Home from "./components/Home";
+import Rooms from "./components/Rooms";
+import SingleRoom from "./components/SingleRoom";
+import Error from "./components/Error";
+import { Route, Link } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+      <Home />
+      <Rooms />
+      <SingleRoom />
+      <Error />
+    </Fragment>
   );
 }
 
