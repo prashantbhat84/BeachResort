f6db445e49d42033f577a3bb511fa6a9b3b3af4e
prashant-freelance <prashant.work.1984@gmail.com>
2020-02-03 12:27:26 +0530

    services  section done with a seperate title component

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,16 +1,23 @@
-import React from "react";
+import React, { Fragment } from "react";
 import Hero from "./Hero/Hero";
 import Banner from "./Banner/Banner";
 import { Link } from "react-router-dom";
+import Services from "./Services/Services";
 
 export default function Home() {
   return (
-    <Hero>
-      <Banner title="Luxurious Rooms" subtitle="Deluxe Rooms starting at $299">
-        <Link to="/rooms" className="btn-primary">
-          Our Rooms
-        </Link>
-      </Banner>
-    </Hero>
+    <Fragment>
+      <Hero>
+        <Banner
+          title="Luxurious Rooms"
+          subtitle="Deluxe Rooms starting at $299"
+        >
+          <Link to="/rooms" className="btn-primary">
+            Our Rooms
+          </Link>
+        </Banner>
+      </Hero>
+      <Services />
+    </Fragment>
   );
 }
--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -1,6 +1,16 @@
 import React from "react";
 import Hero from "./Hero/Hero";
+import Banner from "./Banner/Banner";
+import { Link } from "react-router-dom";
 
 export default function Rooms() {
-  return <Hero hero="roomsHero" />;
+  return (
+    <Hero hero="roomsHero">
+      <Banner title="Our Rooms">
+        <Link to="/" className="btn-primary">
+          Home
+        </Link>
+      </Banner>
+    </Hero>
+  );
 }
