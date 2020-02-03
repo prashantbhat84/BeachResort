6431f477126b18e68f9ef874c8a8cfe12e3fb7f7
prashant-freelance <prashant.work.1984@gmail.com>
2020-02-03 16:56:56 +0530

    Features +navigation to single room

--- src/components/Featuredrooms/FeaturedRooms.js
+++ src/components/Featuredrooms/FeaturedRooms.js
@@ -1,16 +1,27 @@
 import React, { Component } from "react";
 import { RoomContext } from "../context";
 import Loading from "../loading/Loading";
+import Room from "../Room/Room";
+import Title from "../Title/Title";
 
 class FeaturedRooms extends Component {
   static contextType = RoomContext;
   render() {
     // const { rooms } = this.context;
     // console.log(rooms[0]);
-    const { featuredrooms } = this.context;
-    console.log(featuredrooms);
+    let { loading, featuredrooms: rooms } = this.context;
+    rooms = rooms.map(room => {
+      return <Room key={room.id} room={room} />;
+    });
 
-    return <Loading />;
+    return (
+      <section className="featured-rooms">
+        <Title title="Featured Rooms" />
+        <div className="featured-rooms-center">
+          {loading ? <Loading /> : rooms}
+        </div>
+      </section>
+    );
   }
 }
 
--- src/components/Room/Room.js
+++ src/components/Room/Room.js
@@ -1,7 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import defaultImage from "../../images/room-1.jpeg";
 
-const Room = () => {
-  return <div>Hello from Room</div>;
+const Room = ({ room }) => {
+  const { name, slug, price, images } = room;
+  return (
+    <article className="room">
+      <div className="img-container">
+        <img src={images[0] || defaultImage} alt="single room" />
+        <div className="price-top">
+          <h6>${price}</h6>
+          <p>per night</p>
+        </div>
+        <Link to={`/rooms/${slug}`} className="btn-primary room-link">
+          Features
+        </Link>
+      </div>
+    </article>
+  );
 };
 
 export default Room;
