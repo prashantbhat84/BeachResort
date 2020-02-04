61d906de4e4b313421a575ee0c0dbb5f47f64fcd
prashant-freelance <prashant.work.1984@gmail.com>
2020-02-04 15:18:45 +0530

    context for functional components

--- src/components/RoomContainer.js
+++ src/components/RoomContainer.js
@@ -1,24 +1,50 @@
 import React from "react";
 import RoomsFilter from "./RoomsFilter";
 import RoomsList from "./RoomsList";
-import { RoomConsumer } from "./context";
+import { withRoomConsumer } from "./context";
 import Loading from "./loading/Loading";
 
-const RoomContainer = () => {
+const RoomContainer = ({ context }) => {
+  const { loading, sortedRooms, rooms } = context;
+  if (loading) {
+    return <Loading />;
+  }
   return (
-    <RoomConsumer>
-      {value => {
-        const { rooms, loading, sortedRooms } = value;
-        return (
-          <div>
-            Hello from Rooms Container
-            <RoomsFilter />
-            <RoomsList />
-          </div>
-        );
-      }}
-    </RoomConsumer>
+    <div>
+      Hello from Rooms Container
+      <RoomsFilter rooms={rooms} />
+      <RoomsList rooms={sortedRooms} />
+    </div>
   );
 };
 
-export default RoomContainer;
+export default withRoomConsumer(RoomContainer);
+
+// import React from "react";
+// import RoomsFilter from "./RoomsFilter";
+// import RoomsList from "./RoomsList";
+// import { RoomConsumer } from "./context";
+// import Loading from "./loading/Loading";
+
+// const RoomContainer = () => {
+//   return (
+//     <RoomConsumer>
+//       {/*value is a function. This pattern was used before hooks*/}
+//       {value => {
+//         const { rooms, loading, sortedRooms } = value;
+//         if (loading) {
+//           return <Loading />;
+//         }
+//         return (
+//           <div>
+//             Hello from Rooms Container
+//             <RoomsFilter rooms={rooms} />
+//             <RoomsList rooms={sortedRooms} />
+//           </div>
+//         );
+//       }}
+//     </RoomConsumer>
+//   );
+// };
+
+// export default RoomContainer;
