558be777ad32ce1f748c3a6859bcbfd35257ffd9
Prashant Bhat <prashantbhat84@gmail.com>
2020-02-07 14:23:06 +0530

    changed home page route

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ function App() {
     <Fragment>
       <NavBar />
       <Switch>
-        <Route exact path="/" component={Home} />
+        <Route exact path="/BeachResort" component={Home} />
         <Route exact path="/rooms" component={Rooms} />
         <Route exact path="/rooms/:slug" component={SingleRoom} />
         <Route component={Error} />
